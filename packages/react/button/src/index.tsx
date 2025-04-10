125d7d3ef0bd406399d915f8a987fcc983947d98
Nikita Tashchilin <v-ntashchilin@rockethomes.com>
2025-04-10 17:49:37 -0400

    feat: add Slot component and AsChild functionality to button

--- packages/react/button/src/index.tsx
+++ packages/react/button/src/index.tsx
@@ -1,18 +1,56 @@
+import { Slot } from "@rds/react-slot";
 import "@rds/styles/button.css";
 import { mergeClasses } from "@rds/utils";
-import { ButtonHTMLAttributes, Children, forwardRef } from "react";
+import {
+  type ButtonHTMLAttributes,
+  Children,
+  type ComponentRef,
+  ElementType,
+  forwardRef,
+  type ReactElement,
+} from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: "lg" | "md";
   variant?: "primary" | "secondary" | "tertiary" | "warning" | "icon";
+  asChild?: boolean;
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+export const Button = forwardRef<ComponentRef<"button">, ButtonProps>(
   (
-    { className, children, size = "md", variant = "primary", ...props },
+    {
+      className,
+      children,
+      size = "md",
+      variant = "primary",
+      asChild,
+      ...props
+    },
     ref
   ) => {
-    // Wrap text nodes in spans for consistent CSS targeting
+    const buttonClassName = mergeClasses(
+      "rkt-button",
+      size === "lg" && "rkt-button--large",
+      variant === "secondary" && "rkt-button--secondary",
+      variant === "tertiary" && "rkt-button--tertiary",
+      variant === "warning" && "rkt-button--warning",
+      variant === "icon" && "rkt-button--icon",
+      className
+    );
+
+    const buttonProps = {
+      ...props,
+      className: buttonClassName,
+      "data-size": size,
+      "data-variant": variant,
+      ref,
+    };
+
+    if (asChild && Children.count(children) === 1) {
+      const child = Children.only(children) as ReactElement<any, ElementType>;
+      return <Slot {...buttonProps}>{child}</Slot>;
+    }
+
     const wrappedChildren = Children.map(children, (child) => {
       if (typeof child === "string" || typeof child === "number") {
         return <span>{child}</span>;
@@ -20,25 +58,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       return child;
     });
 
-    return (
-      <button
-        className={mergeClasses(
-          "rkt-button",
-          size === "lg" && "rkt-button--large",
-          variant === "secondary" && "rkt-button--secondary",
-          variant === "tertiary" && "rkt-button--tertiary",
-          variant === "warning" && "rkt-button--warning",
-          variant === "icon" && "rkt-button--icon",
-          className
-        )}
-        ref={ref}
-        {...props}
-        data-size={size}
-        data-variant={variant}
-      >
-        {wrappedChildren}
-      </button>
-    );
+    return <button {...buttonProps}>{wrappedChildren}</button>;
   }
 );
 
---
+++ packages/react/slot/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => ({
+  entryPoints: ['src/index.tsx'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  external: ['react'],
+  
+  ...options,
+}));
